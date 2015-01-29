c4f0591c9c5a429acacee0d598c9494d7606303d
Will Schive <lexigram@goodworksonearth.org>
2015-01-29 12:04:39 -0800

    fixed configuration deprecation, final products_controller_spec.rb test passes

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,4 +1,4 @@
-describe ProductsController do
+describe ProductsController, type: :controller do
 
   describe 'GET #show' do
 
@@ -7,11 +7,15 @@ describe ProductsController do
       expect(response.headers['Content-Type']).to have_content 'application/pdf'
     end
 
-    it "fails to return a PDF file" do
-      get :show, id: 'misnamed_product', format: 'xml'
+    it "fails to return a PDF formatted file" do
+      get :show, id: 'product', format: 'xml'
       expect(response.headers['Content-Type']).not_to have_content 'application/pdf'
     end
 
+    it "fails to return an improperly named file" do
+      get :show, id: '3a4b', format: 'pdf'
+      expect(response.body).to match(/You need to sign in or sign up before continuing/)
+    end
   end
 
 end
